7f33e6b6ea8b0de9e364d59b97019c4305313148
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-22 00:45:15 -0700

    added data format, has time in format, and format of the component

--- src/main/java/org/aadarshdevi/hardwaremonitor/backend/data/Component.java
+++ src/main/java/org/aadarshdevi/hardwaremonitor/backend/data/Component.java
@@ -2,6 +2,11 @@ package org.aadarshdevi.hardwaremonitor.backend.data;
 
 public class Component {
     String name;
-    String folderPath;
     String description;
+    String dataFormat;
+    boolean hasTime;
+    int formatNum;
 }
+
+// my-motor=12,91,56,78
+// ir-sensor=12
