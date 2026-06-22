f723c39135b66463dd0ba15c239c25cf99798d02
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-21 20:37:10 -0700

    home ui controller

---
+++ src/main/java/org/aadarshdevi/hardwaremonitor/frontend/controller/HomeController.java
@@ -0,0 +1,9 @@
+package org.aadarshdevi.hardwaremonitor.frontend.controller;
+
+import org.aadarshdevi.hardwaremonitor.backend.data.Project;
+
+import java.util.List;
+
+public class HomeController {
+    List<Project> projectList;
+}
