b606fc187456c1a09906f2c51d262c661bf6148a
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-21 22:59:29 -0700

    OS not found exception

---
+++ src/main/java/org/aadarshdevi/hardwaremonitor/backend/setup/exception/OperatingSystemNotFoundException.java
@@ -0,0 +1,7 @@
+package org.aadarshdevi.hardwaremonitor.backend.setup.exception;
+
+public class OperatingSystemNotFoundException extends RuntimeException {
+    public OperatingSystemNotFoundException(String message) {
+        super(message);
+    }
+}
