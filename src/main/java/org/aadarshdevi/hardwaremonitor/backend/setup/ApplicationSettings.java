0743a5f4382caf93bc67a05b0962fb36d36c88a3
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-21 22:58:01 -0700

    settings class

---
+++ src/main/java/org/aadarshdevi/hardwaremonitor/backend/setup/ApplicationSettings.java
@@ -0,0 +1,36 @@
+package org.aadarshdevi.hardwaremonitor.backend.setup;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.nio.file.Path;
+
+public class ApplicationSettings {
+    private static ApplicationSettings instance;
+    @Setter
+    @Getter
+    private final Version version = new Version("0.1.0");
+    @Setter
+    @Getter
+    private OperatingSystem.OSType operatingSystem;
+    @Setter
+    @Getter
+    private Path appRootFolder;
+    @Setter
+    @Getter
+    private Path projectsRootFolder;
+    @Setter
+    @Getter
+    private Path settingsFile;
+    @Setter
+    @Getter
+    private Path logFile;
+    @Setter
+    @Getter
+    private Path componentsFolder;
+
+    public static ApplicationSettings getInstance() {
+        if (instance == null) instance = new ApplicationSettings();
+        return instance;
+    }
+}
