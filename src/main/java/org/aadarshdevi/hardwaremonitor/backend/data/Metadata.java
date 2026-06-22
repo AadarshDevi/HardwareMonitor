2f357477b3ecd513cf1e4506dc7e401704e15c09
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-21 20:37:58 -0700

    metadata class

---
+++ src/main/java/org/aadarshdevi/hardwaremonitor/backend/data/Metadata.java
@@ -0,0 +1,13 @@
+package org.aadarshdevi.hardwaremonitor.backend.data;
+
+import java.time.LocalDateTime;
+
+public class Metadata {
+    String name;
+    LocalDateTime created;
+    LocalDateTime modified;
+    LocalDateTime opened;
+
+    public Metadata() {
+    }
+}
