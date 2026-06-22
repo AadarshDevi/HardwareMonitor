aa41a1d44aad6f600cb06ab310c48a8594f68747
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-22 00:46:13 -0700

    delete unnecessary classes (child classes of component)

--- src/main/java/org/aadarshdevi/hardwaremonitor/backend/data/component/InertialMeasurementUnit.java
+++
@@ -1,6 +0,0 @@
-package org.aadarshdevi.hardwaremonitor.backend.data.component;
-
-import org.aadarshdevi.hardwaremonitor.backend.data.Component;
-
-public class InertialMeasurementUnit extends Component {
-}
