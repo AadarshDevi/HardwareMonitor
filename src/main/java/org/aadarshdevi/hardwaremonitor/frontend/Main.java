6f89ae1a19d022e3907838b665137f9a7b49ee9d
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-06-21 23:56:33 -0700

    need to add ui

--- src/main/java/org/aadarshdevi/hardwaremonitor/frontend/Main.java
+++ src/main/java/org/aadarshdevi/hardwaremonitor/frontend/Main.java
@@ -18,6 +18,8 @@ public class Main extends Application {
         as.findLog();
         as.findComponents();
         List<Project> projects = as.getProjects();
+
+        // add projects to ui
     }
 }
 
